3bc86049ca2adbbcd426c951d45067d3c314ae55
Ubuntu <ubuntu@ip-172-31-11-28.ec2.internal>
2020-07-26 18:56:50 +0000

    res

--- replaying/test.py
+++ replaying/test.py
@@ -12,10 +12,11 @@ from sklearn.model_selection import StratifiedKFold
 from math import log2, ceil 
 
 import sys
-sys.path.append("../src/")
+sys.path.append("../src_new/")
 from lifelong_dnn import LifeLongDNN
+from joblib import Parallel, delayed
 
-# %%
+#%%
 def unpickle(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
@@ -80,24 +81,337 @@ def generate_gaussian_parity(n, mean=np.array([-1, -1]), cov_scale=1, angle_para
     return X, Y.astype(int)
 
 
+#%%
+def experiment(n_xor, n_nxor, n_test, reps, n_trees, max_depth, acorn=None):
+    #print(1)
+    if n_xor==0 and n_nxor==0:
+        raise ValueError('Wake up and provide samples to train!!!')
+    
+    if acorn != None:
+        np.random.seed(acorn)
+    
+    errors = np.zeros((reps,4),dtype=float)
+    
+    for i in range(reps):
+        l2f = LifeLongDNN(parallel=False)
+        uf = LifeLongDNN(parallel=False)
+        #source data
+        xor, label_xor = generate_gaussian_parity(n_xor,cov_scale=0.1,angle_params=0)
+        test_xor, test_label_xor = generate_gaussian_parity(n_test,cov_scale=0.1,angle_params=0)
+
+        min_xor = np.min(xor)
+        xor = (xor - min_xor)
+        max_xor = np.max(xor)
+        xor = xor/max_xor
+        test_xor = (test_xor-min_xor)/max_xor
+        #target data
+        if n_nxor!=0:
+            nxor, label_nxor = generate_gaussian_parity(n_nxor,cov_scale=0.1,angle_params=np.pi/2)
+            test_nxor, test_label_nxor = generate_gaussian_parity(n_test,cov_scale=0.1,angle_params=np.pi/2)
+
+            min_nxor = np.min(nxor)
+            nxor = (nxor - min_nxor)
+            max_nxor = np.max(nxor)
+            nxor = nxor/max_nxor
+            test_nxor = (test_nxor-min_nxor)/max_nxor
+
+        if n_xor == 0:
+            l2f.new_forest(nxor, label_nxor, n_estimators=n_trees,max_depth=max_depth)
+            
+            errors[i,0] = 0.5
+            errors[i,1] = 0.5
+            
+            uf_task2=l2f.predict(test_nxor, representation=0, decider=0)
+            l2f_task2=l2f.predict(test_nxor, representation='all', decider=0)
+            
+            errors[i,2] = 1 - np.sum(uf_task2 == test_label_nxor)/n_test
+            errors[i,3] = 1 - np.sum(l2f_task2 == test_label_nxor)/n_test
+        elif n_nxor == 0:
+            l2f.new_forest(xor, label_xor, n_estimators=n_trees,max_depth=max_depth)
+            
+            uf_task1=l2f.predict(test_xor, representation=0, decider=0)
+            l2f_task1=l2f.predict(test_xor, representation='all', decider=0)
+            
+            errors[i,0] = 1 - np.sum(uf_task1 == test_label_xor)/n_test
+            errors[i,1] = 1 - np.sum(l2f_task1 == test_label_xor)/n_test
+            errors[i,2] = 0.5
+            errors[i,3] = 0.5
+        else:
+            l2f.new_forest(xor, label_xor, n_estimators=n_trees,max_depth=max_depth)
+            l2f.new_forest(nxor, label_nxor, n_estimators=n_trees,max_depth=max_depth)
+            
+            uf.new_forest(xor, label_xor, n_estimators=n_trees,max_depth=max_depth)
+            uf.new_forest(nxor, label_nxor, n_estimators=n_trees,max_depth=max_depth)
+
+            uf_task1=uf.predict(test_xor, representation=0, decider=0)
+            l2f_task1=l2f.predict(test_xor, representation='all', decider=0)
+            uf_task2=uf.predict(test_nxor, representation=1, decider=1)
+            l2f_task2=l2f.predict(test_nxor, representation='all', decider=1)
+            
+            errors[i,0] = 1 - np.sum(uf_task1 == test_label_xor)/n_test
+            errors[i,1] = 1 - np.sum(l2f_task1 == test_label_xor)/n_test
+            errors[i,2] = 1 - np.sum(uf_task2 == test_label_nxor)/n_test
+            errors[i,3] = 1 - np.sum(l2f_task2 == test_label_nxor)/n_test
+
+    return np.mean(errors,axis=0)
+
+#%%
+mc_rep = 1000
+n_test = 1000
+n_trees = 10
+n_xor = (100*np.arange(0.5, 7.25, step=0.25)).astype(int)
+n_nxor = (100*np.arange(0.5, 7.50, step=0.25)).astype(int)
+
+mean_error = np.zeros((4, len(n_xor)+len(n_nxor)))
+std_error = np.zeros((4, len(n_xor)+len(n_nxor)))
+
+mean_te = np.zeros((2, len(n_xor)+len(n_nxor)))
+std_te = np.zeros((2, len(n_xor)+len(n_nxor)))
+
+for i,n1 in enumerate(n_xor):
+    print('starting to compute %s xor\n'%n1)
+    error = np.array(
+        Parallel(n_jobs=-1,verbose=1)(
+        delayed(experiment)(n1,0,n_test,1,n_trees=n_trees,max_depth=ceil(log2(750))) for _ in range(mc_rep)
+    )
+    )
+    mean_error[:,i] = np.mean(error,axis=0)
+    std_error[:,i] = np.std(error,ddof=1,axis=0)
+    mean_te[0,i] = np.mean(error[:,0]/error[:,1])
+    mean_te[1,i] = np.mean(error[:,2]/error[:,3])
+    std_te[0,i] = np.std(error[:,0]/error[:,1],ddof=1)
+    std_te[1,i] = np.std(error[:,2]/error[:,3],ddof=1)
+    
+    if n1==n_xor[-1]:
+        for j,n2 in enumerate(n_nxor):
+            print('starting to compute %s nxor\n'%n2)
+            
+            error = np.array(
+                Parallel(n_jobs=-1,verbose=1)(
+                delayed(experiment)(n1,n2,n_test,1,n_trees=n_trees,max_depth=ceil(log2(750))) for _ in range(mc_rep)
+            )
+            )
+            mean_error[:,i+j+1] = np.mean(error,axis=0)
+            std_error[:,i+j+1] = np.std(error,ddof=1,axis=0)
+            mean_te[0,i+j+1] = np.mean(error[:,0]/error[:,1])
+            mean_te[1,i+j+1] = np.mean(error[:,2]/error[:,3])
+            std_te[0,i+j+1] = np.std(error[:,0]/error[:,1],ddof=1)
+            std_te[1,i+j+1] = np.std(error[:,2]/error[:,3],ddof=1)
+            
+with open('./result/mean_xor_nxor.pickle','wb') as f:
+    pickle.dump(mean_error,f)
+    
+with open('./result/std_xor_nxor.pickle','wb') as f:
+    pickle.dump(std_error,f)
+    
+with open('./result/mean_te_xor_nxor.pickle','wb') as f:
+    pickle.dump(mean_te,f)
+    
+with open('./result/std_te_xor_nxor.pickle','wb') as f:
+    pickle.dump(std_te,f)
+
+#%% Plotting the result
+#mc_rep = 50
+mean_error = unpickle('result/mean_xor_nxor.pickle')
+std_error = unpickle('result/std_xor_nxor.pickle')
+
+n_xor = (100*np.arange(0.5, 7.25, step=0.25)).astype(int)
+n_nxor = (100*np.arange(0.5, 7.50, step=0.25)).astype(int)
+
+n1s = n_xor
+n2s = n_nxor
+
+ns = np.concatenate((n1s, n2s + n1s[-1]))
+ls=['-', '--']
+algorithms = ['Uncertainty Forest', 'Lifelong Forest']
+
+
+TASK1='XOR'
+TASK2='N-XOR'
+
+fontsize=30
+labelsize=28
+
+colors = sns.color_palette("Set1", n_colors = 2)
+
+fig = plt.figure(constrained_layout=True,figsize=(21,14))
+gs = fig.add_gridspec(14, 21)
+ax1 = fig.add_subplot(gs[7:,:6])
+# for i, algo in enumerate(algorithms):
+ax1.plot(ns, mean_error[0], label=algorithms[0], c=colors[1], ls=ls[np.sum(0 > 1).astype(int)], lw=3)
+#ax1.fill_between(ns, 
+#        mean_error[0] + 1.96*std_error[0], 
+#        mean_error[0] - 1.96*std_error[0], 
+#        where=mean_error[0] + 1.96*std_error[0] >= mean_error[0] - 1.96*std_error[0], 
+#        facecolor=colors[1], 
+#        alpha=0.15,
+#        interpolate=True)
+
+ax1.plot(ns, mean_error[1], label=algorithms[1], c=colors[0], ls=ls[np.sum(1 > 1).astype(int)], lw=3)
+#ax1.fill_between(ns, 
+#        mean_error[1] + 1.96*std_error[1, ], 
+#        mean_error[1] - 1.96*std_error[1, ], 
+#        where=mean_error[1] + 1.96*std_error[1] >= mean_error[1] - 1.96*std_error[1], 
+#        facecolor=colors[0], 
+#        alpha=0.15,
+#        interpolate=True)
+
+ax1.set_ylabel('Generalization Error (%s)'%(TASK1), fontsize=fontsize)
+ax1.legend(loc='upper right', fontsize=20, frameon=False)
+ax1.set_ylim(0.1, 0.21)
+ax1.set_xlabel('Total Sample Size', fontsize=fontsize)
+ax1.tick_params(labelsize=labelsize)
+ax1.set_yticks([0.15, 0.2])
+ax1.set_xticks([250,750,1500])
+ax1.axvline(x=750, c='gray', linewidth=1.5, linestyle="dashed")
+ax1.set_title('XOR', fontsize=30)
+
+right_side = ax1.spines["right"]
+right_side.set_visible(False)
+top_side = ax1.spines["top"]
+top_side.set_visible(False)
+
+ax1.text(400, np.mean(ax1.get_ylim()), "%s"%(TASK1), fontsize=26)
+ax1.text(900, np.mean(ax1.get_ylim()), "%s"%(TASK2), fontsize=26)
+
+#plt.tight_layout()
+
+#plt.savefig('./result/figs/generalization_error_xor.pdf',dpi=500)
+
+#####
+mean_error = unpickle('result/mean_xor_nxor.pickle')
+std_error = unpickle('result/std_xor_nxor.pickle')
+
+algorithms = ['Uncertainty Forest', 'Lifelong Forest']
+
+TASK1='XOR'
+TASK2='N-XOR'
+
+ax1 = fig.add_subplot(gs[7:,7:13])
+# for i, algo in enumerate(algorithms):
+ax1.plot(ns[len(n1s):], mean_error[2, len(n1s):], label=algorithms[0], c=colors[1], ls=ls[1], lw=3)
+#ax1.fill_between(ns[len(n1s):], 
+#        mean_error[2, len(n1s):] + 1.96*std_error[2, len(n1s):], 
+#        mean_error[2, len(n1s):] - 1.96*std_error[2, len(n1s):], 
+#        where=mean_error[2, len(n1s):] + 1.96*std_error[2, len(n1s):] >= mean_error[2, len(n1s):] - 1.96*std_error[2, len(n1s):], 
+#        facecolor=colors[1], 
+#        alpha=0.15,
+#        interpolate=True)
+
+ax1.plot(ns[len(n1s):], mean_error[3, len(n1s):], label=algorithms[1], c=colors[0], ls=ls[1], lw=3)
+#ax1.fill_between(ns[len(n1s):], 
+#        mean_error[3, len(n1s):] + 1.96*std_error[3, len(n1s):], 
+#        mean_error[3, len(n1s):] - 1.96*std_error[3, len(n1s):], 
+#        where=mean_error[3, len(n1s):] + 1.96*std_error[3, len(n1s):] >= mean_error[3, len(n1s):] - 1.96*std_error[3, len(n1s):], 
+#        facecolor=colors[0], 
+#        alpha=0.15,
+#        interpolate=True)
+
+ax1.set_ylabel('Generalization Error (%s)'%(TASK2), fontsize=fontsize)
+ax1.legend(loc='upper right', fontsize=20, frameon=False)
+#         ax1.set_ylim(-0.01, 0.22)
+ax1.set_xlabel('Total Sample Size', fontsize=fontsize)
+ax1.tick_params(labelsize=labelsize)
+# ax1.set_yticks([0.15, 0.25, 0.35])
+ax1.set_yticks([0.15, 0.2])
+ax1.set_xticks([250,750,1500])
+ax1.axvline(x=750, c='gray', linewidth=1.5, linestyle="dashed")
+
+ax1.set_ylim(0.11, 0.21)
+
+ax1.set_xlim(-10)
+right_side = ax1.spines["right"]
+right_side.set_visible(False)
+top_side = ax1.spines["top"]
+top_side.set_visible(False)
+
+# ax1.set_ylim(0.14, 0.36)
+ax1.text(400, np.mean(ax1.get_ylim()), "%s"%(TASK1), fontsize=26)
+ax1.text(900, np.mean(ax1.get_ylim()), "%s"%(TASK2), fontsize=26)
+
+ax1.set_title('N-XOR', fontsize=30)
+#plt.tight_layout()
+
+#plt.savefig('./result/figs/generalization_error_nxor.pdf',dpi=500)
+
+#####
+mean_error = unpickle('result/mean_te_xor_nxor.pickle')
+std_error = unpickle('result/std_te_xor_nxor.pickle')
+
+algorithms = ['Backward Transfer', 'Forward Transfer']
+
+TASK1='XOR'
+TASK2='N-XOR'
+
+ax1 = fig.add_subplot(gs[7:,14:])
+
+ax1.plot(ns, mean_error[0], label=algorithms[0], c=colors[0], ls=ls[0], lw=3)
+#ax1.fill_between(ns, 
+#        mean_error[0] + 1.96*std_error[0], 
+#        mean_error[0] - 1.96*std_error[0], 
+#        where=mean_error[1] + 1.96*std_error[0] >= mean_error[0] - 1.96*std_error[0], 
+#        facecolor=colors[0], 
+#        alpha=0.15,
+#        interpolate=True)
+
+ax1.plot(ns[len(n1s):], mean_error[1, len(n1s):], label=algorithms[1], c=colors[0], ls=ls[1], lw=3)
+#ax1.fill_between(ns[len(n1s):], 
+#        mean_error[1, len(n1s):] + 1.96*std_error[1, len(n1s):], 
+#        mean_error[1, len(n1s):] - 1.96*std_error[1, len(n1s):], 
+#        where=mean_error[1, len(n1s):] + 1.96*std_error[1, len(n1s):] >= mean_error[1, len(n1s):] - 1.96*std_error[1, len(n1s):], 
+#        facecolor=colors[0], 
+#        alpha=0.15,
+#        interpolate=True)
+
+ax1.set_ylabel('Transfer Efficiency', fontsize=fontsize)
+ax1.legend(loc='upper right', fontsize=20, frameon=False)
+ax1.set_ylim(.99, 1.4)
+ax1.set_xlabel('Total Sample Size', fontsize=fontsize)
+ax1.tick_params(labelsize=labelsize)
+ax1.set_yticks([1,1.2,1.4])
+ax1.set_xticks([250,750,1500])
+ax1.axvline(x=750, c='gray', linewidth=1.5, linestyle="dashed")
+right_side = ax1.spines["right"]
+right_side.set_visible(False)
+top_side = ax1.spines["top"]
+top_side.set_visible(False)
+ax1.hlines(1, 50,1500, colors='gray', linestyles='dashed',linewidth=1.5)
+
+ax1.text(400, np.mean(ax1.get_ylim()), "%s"%(TASK1), fontsize=26)
+ax1.text(900, np.mean(ax1.get_ylim()), "%s"%(TASK2), fontsize=26)
+
+#plt.tight_layout()
+
+#plt.savefig('./result/figs/TE.pdf',dpi=500)
+
+#####
+colors = sns.color_palette('Dark2', n_colors=2)
+
+X, Y = generate_gaussian_parity(750, cov_scale=0.1, angle_params=0)
+Z, W = generate_gaussian_parity(750, cov_scale=0.1, angle_params=np.pi/2)
+
+ax = fig.add_subplot(gs[:6,4:10])
+ax.scatter(X[:, 0], X[:, 1], c=get_colors(colors, Y), s=50)
+
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_title('Gaussian XOR', fontsize=30)
+
+plt.tight_layout()
+ax.axis('off')
+#plt.savefig('./result/figs/gaussian-xor.pdf')
+
+###
+colors = sns.color_palette('Dark2', n_colors=2)
+
+ax = fig.add_subplot(gs[:6,11:16])
+ax.scatter(Z[:, 0], Z[:, 1], c=get_colors(colors, W), s=50)
+
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_title('Gaussian N-XOR', fontsize=30)
+ax.axis('off')
+#plt.tight_layout()
+plt.savefig('./result/figs/xor_nxor_exp.pdf')
 
 # %%
-xor, label_xor = generate_gaussian_parity(100,cov_scale=0.1,angle_params=0)
-test_xor, test_label_xor = generate_gaussian_parity(10,cov_scale=0.1,angle_params=0)
-
-nxor, label_nxor = generate_gaussian_parity(1000,cov_scale=0.1,angle_params=np.pi/2)
-test_nxor, test_label_nxor = generate_gaussian_parity(10,cov_scale=0.1,angle_params=np.pi/2)
-
-xor = (xor - np.min(xor))
-xor = xor/np.max(xor)
-
-l2f = LifeLongDNN(parallel=False)
-l2f.new_forest(xor, label_xor, n_estimators=10,max_depth=10)
-l2f.new_forest(nxor, label_nxor, n_estimators=10,max_depth=10)
-
-l2f_task1 = l2f.predict(test_xor, representation='all', decider=0)
-uf_task1 = l2f.predict(test_xor, representation=0, decider=0)
-
-print(np.mean(uf_task1 == test_label_xor))
-print(np.mean(l2f_task1 == test_label_xor))
- # %%
