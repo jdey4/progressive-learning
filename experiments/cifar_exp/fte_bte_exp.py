6153c68163cf7c87d322db66a8ddbdb88f081b61
j1c <jdey4@jhmi.edu>
2020-07-29 21:31:33 -0400

    codes changed

--- experiments/cifar_exp/fte_bte_exp.py
+++ experiments/cifar_exp/fte_bte_exp.py
@@ -13,7 +13,7 @@ from sklearn.model_selection import StratifiedKFold
 from math import log2, ceil 
 
 import sys
-sys.path.append("../../src/")
+sys.path.append("../../src_mapping_2/")
 from lifelong_dnn import LifeLongDNN
 from joblib import Parallel, delayed
 from multiprocessing import Pool
