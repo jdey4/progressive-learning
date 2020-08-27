0cb5c176cac952e73e160b68b645636e5592fa9a
j1c <jdey4@jhmi.edu>
2020-08-26 22:00:09 -0400

    sufficient conditions

--- replaying/sufficient_condition_test.py
+++ replaying/sufficient_condition_test.py
@@ -14,8 +14,8 @@ from sklearn.model_selection import StratifiedKFold
 from math import log2, ceil 
 
 import sys
-#sys.path.append("../src/")
-sys.path.append("../src_mapping_1/")
+sys.path.append("../src/")
+#sys.path.append("../src_mapping_1/")
 from lifelong_dnn import LifeLongDNN
 from joblib import Parallel, delayed
 from multiprocessing import Pool
