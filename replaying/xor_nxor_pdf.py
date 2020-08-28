378399b5870a25bff4f146a1cbe7f4d4b2a27896
j1c <jdey4@jhmi.edu>
2020-08-28 13:45:03 -0400

    true pdf

--- replaying/xor_nxor_pdf.py
+++ replaying/xor_nxor_pdf.py
@@ -64,3 +64,46 @@ ax.invert_yaxis()
 plt.savefig('result/figs/true_pdf.pdf')
 
 # %%
+def generate_2d_rotation(theta=0, acorn=None):
+    if acorn is not None:
+        np.random.seed(acorn)
+    
+    R = np.array([
+        [np.cos(theta), np.sin(theta)],
+        [-np.sin(theta), np.cos(theta)]
+    ])
+    
+    return R
+    
+# %%
+delta = 0.01
+x = np.arange(-1,1,step=delta)
+y = np.arange(-1,1,step=delta)
+x,y = np.meshgrid(x,y)
+sample = np.concatenate(
+            (
+                x.reshape(-1,1),
+                y.reshape(-1,1)
+            ),
+            axis=1
+) 
+
+z = np.zeros(len(sample),dtype=float)
+R = generate_2d_rotation(theta=np.pi*45/180)
+for ii,x in enumerate(sample):
+    z[ii] = pdf(R@x)
+
+data = pd.DataFrame(data={'x':sample[:,0], 'y':sample[:,1], 'z':z})
+data = data.pivot(index='x', columns='y', values='z')
+
+sns.set_context("talk")
+fig, ax = plt.subplots(1,1, figsize=(8,8))
+cmap= sns.diverging_palette(240, 10, n=9)
+ax1 = sns.heatmap(data, ax=ax, vmin=0, vmax=1,cmap=cmap)
+ax1.set_xticklabels(['-1','' , '', '', '', '', '','','','','0','','','','','','','','','1'])
+ax1.set_yticklabels(['-1','' , '', '', '', '', '','','','','','','0','','','','','','','','','','','','1'])
+#ax1.set_yticklabels(['-1','' , '', '', '', '', '','','','' , '', '', '', '', '', '','','','','', '0','','' , '', '', '', '', '','','','','','','','','','','','','1'])
+ax.set_title('True PDF of xor-nxor simulation data',fontsize=24)
+ax.invert_yaxis()
+plt.savefig('result/figs/true_pdf_xor_rxor.pdf')
+# %%
